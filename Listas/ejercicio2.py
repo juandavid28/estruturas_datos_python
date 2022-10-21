c9fd635defb4fc26a5902d4a0c78f5c43e188456
juandavid28 <juanrodrigue1@colegioguanenta.edu.co>
2022-10-21 07:21:42 -0500

    Se agregaron ejemplos de la lista

---
+++ Listas/ejercicio2.py
@@ -0,0 +1,36 @@
+# EJERCICIOS LISTAS
+
+#Metodos propios
+
+lista = [45, 32, 3, 78]
+print("lista original: ", lista)
+# Funcion append: añande un elemento a la lista
+lista.append(995)
+lista.append(7)
+print("Lidta despues de usar append: ", lista)
+# Funcion sort: ordena la lista
+lista.sort()
+print("Lista ordenada:", lista)
+# Funcion reverse: Invierte el orden de la lista
+lista.reverse()
+print("Lista al reves: ", lista)
+# Funcion extend: añade los elmentos de una lista a la lista
+lista_extend = [1,5,87,45]
+lista.extend(lista_extend)
+print("Lista de extend", lista)
+# FUncion count: cuenta el numero de veces que aparece el elemento indicado como parametro dentro de la lista
+print("Numero de elementos 45: ", lista.count)
+# Funcion inset: añade el elemento pasado como parametro a la lista en la posicion indicada tambien por parametro
+lista.insert(4,111)
+print("Lista despues de insert: ", lista)
+# Funcion remove: elimina la primera ocurrencia empezada por la izquierda de la lista del elemento indicado como parametro
+lista.remove(45)
+print("LIsta despues de rmove:", lista)
+# Funcion index: Devuelve la posicion de la primera ocurrencia de izquierda a derecha en la lista, del elemento pasado como parametro
+print("Posicion del elemeto 111: ", lista.index(111))
+# Funcion pop: Elimina el ultimo elemento de la lista y lo delvuelve como resultado de la operacion
+lista.pop()
+print("Lista despues de pop: ", lista)
+# Funcion clear: Elimina tosdos los elementos de la lista
+lista.clear()
+print("Lisa despues de clear: ", lista)
--- Listas/ejercicios.py
+++ Listas/ejercicios.py
@@ -36,3 +36,30 @@ lista[1] = "Cazadora"
 print(lista)
 del lista[0]
 print(lista)
+
+# 5. Uso del operador *. Permite concatenar una lista con ella misma un numero finito de veces.
+lista = ["Camiseta", "Pantalon", "Zapatillas"]
+print(lista)
+lista_resultante = lista * 3
+print(lista_resultante)
+
+# 6. Creacion de listas como elementos de listas y accesos a dichos elementos.
+print("-----Ejercicios 6 -----")
+lista = ["Camiseta", ["Calcetines", "Cazadores"], "Zapatillas"]
+print(lista)
+print(lista[0])
+print(lista[1])
+print(lista[2])
+print(lista[1][0])
+print(lista[1][1])
+
+# 7. Estraer una porcion de la lista en una lista nueva
+print("-----Ejercicios 7 -----")
+lista = [1,2,3,4,5,6,7,8,9]
+print(lista)
+lista1 = lista[3:7]
+print(lista1)
+lista2 = lista[:5]
+print(lista2)
+lista3 = lista[6:]
+print(lista3)
